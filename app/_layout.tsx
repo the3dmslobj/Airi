cd7ebfe055bbb6425fbfd30bcf99f0910b885fec
the3dmslobj <danielisthuta.3@gmail.com>
2026-05-30 18:13:55 +0700

    feat: load Silkscreen and VT323 pixel fonts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,3 +1,8 @@
+import {
+  Silkscreen_400Regular,
+  Silkscreen_700Bold,
+} from "@expo-google-fonts/silkscreen";
+import { VT323_400Regular } from "@expo-google-fonts/vt323";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
@@ -18,12 +23,9 @@ const queryClient = new QueryClient();
 
 export default function RootLayout() {
   const [fontsLoaded, fontError] = useFonts({
-    "bri-bold": require("../assets/fonts/Bricolage_Grotesque/BricolageGrotesque-Bold.ttf"),
-    "dm-bold": require("../assets/fonts/DM_Sans/DMSans-Bold.ttf"),
-    "dm-light": require("../assets/fonts/DM_Sans/DMSans-Light.ttf"),
-    "dm-medium": require("../assets/fonts/DM_Sans/DMSans-Medium.ttf"),
-    "dm-semi": require("../assets/fonts/DM_Sans/DMSans-SemiBold.ttf"),
-    "dm-semiItalic": require("../assets/fonts/DM_Sans/DMSans-SemiBoldItalic.ttf"),
+    Silkscreen_400Regular,
+    Silkscreen_700Bold,
+    VT323_400Regular,
   });
 
   useEffect(() => {
