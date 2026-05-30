4cff90f6205189a368b49de317a091b32d01f8fe
the3dmslobj <danielisthuta.3@gmail.com>
2026-05-30 17:49:43 +0700

    feat: add star toggle to save cities from search results

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,5 +1,7 @@
 import { CityResult } from "@/interfaces/city.interface";
 import { geocodeCityQueryOptions } from "@/services/queryOptions";
+import { addLocation, removeLocation } from "@/slices/savedLocationsSlice";
+import { AppDispatch, RootState } from "@/store/store";
 import { Ionicons } from "@expo/vector-icons";
 import { useQuery } from "@tanstack/react-query";
 import { useLocalSearchParams, useRouter } from "expo-router";
@@ -11,9 +13,14 @@ import {
   TouchableOpacity,
   View,
 } from "react-native";
+import { useDispatch, useSelector } from "react-redux";
 
 const SearchBar = () => {
   const router = useRouter();
+  const dispatch = useDispatch<AppDispatch>();
+  const savedItems = useSelector(
+    (state: RootState) => state.savedLocations.items
+  );
   const [searchQuery, setSearchQuery] = useState<string | null>(null);
 
   const { query } = useLocalSearchParams();
@@ -63,24 +70,59 @@ const SearchBar = () => {
         <View className="flex flex-col bg-n800 absolute top-36 w-full rounded-xl mt-2 p-2 z-10 border-[1px] border-n700 gap-1">
           {!isPending ? (
             cities?.length !== 0 ? (
-              cities?.map((city, index) => (
-                <TouchableOpacity
-                  onPress={() => {
-                    router.setParams({
-                      lat: city.latitude,
-                      lon: city.longitude,
-                    });
-                    setSearchQuery("");
-                    router.setParams({ query: "" });
-                  }}
-                  className="px-2 py-2.5 rounded-lg"
-                  key={index}
-                >
-                  <Text className="text-xl font-dm text-n0">
-                    {city?.name + ", " + city?.country}
-                  </Text>
-                </TouchableOpacity>
-              ))
+              cities?.map((city) => {
+                const isSaved = savedItems.some((item) => item.id === city.id);
+
+                return (
+                  <View
+                    key={city.id}
+                    className="flex flex-row items-center px-2 py-2.5 rounded-lg gap-2"
+                  >
+                    <TouchableOpacity
+                      className="flex-1"
+                      onPress={() => {
+                        router.setParams({
+                          lat: city.latitude,
+                          lon: city.longitude,
+                          name: city.name,
+                          country: city.country,
+                          query: "",
+                        });
+                        setSearchQuery("");
+                      }}
+                    >
+                      <Text className="text-xl font-dm text-n0">
+                        {city?.name + ", " + city?.country}
+                      </Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity
+                      hitSlop={8}
+                      onPress={() => {
+                        if (isSaved) {
+                          dispatch(removeLocation(city.id));
+                        } else {
+                          dispatch(
+                            addLocation({
+                              id: city.id,
+                              name: city.name,
+                              country: city.country,
+                              latitude: city.latitude,
+                              longitude: city.longitude,
+                            })
+                          );
+                        }
+                      }}
+                    >
+                      <Ionicons
+                        name={isSaved ? "star" : "star-outline"}
+                        size={22}
+                        color={isSaved ? "#facc15" : "white"}
+                      />
+                    </TouchableOpacity>
+                  </View>
+                );
+              })
             ) : (
               <Text className="text-n0 text-center font-dmBold text-xl py-2.5">
                 Sorry, we can't find the city.
